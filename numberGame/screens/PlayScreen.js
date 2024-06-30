cb7d25824602f9e51169207214952ae68e0a7cde
Aparna-xxx <aparnaramanathan14@gmail.com>
2024-06-30 18:02:59 +0530

    added guess display

--- numberGame/screens/PlayScreen.js
+++ numberGame/screens/PlayScreen.js
@@ -1,13 +1,32 @@
 import { Text,StyleSheet,View} from "react-native";
 import Title from "../components/Title";
 import Colors from "../util/Colors";
-function PlayScreen(){
+import { useState } from "react";
+
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
+function PlayScreen(props){
+
+    const firstNum= generateRandomBetween(1,100, props.enteredValue)
+    const [guess,SetGuess]=useState(firstNum);
 
     return(
         <View style={styles.parentContainer}>
             <View style={styles.textContainer}>
-                <Title>Opponent's Guess</Title>
+                <Title>Opponent's Guess</Title>               
             </View>
+            <View style={styles.guessContainer}>
+                    <Text style={styles.guessTextStyle}>{guess}</Text>
+                </View>
         </View>
     )
 }
@@ -33,5 +52,18 @@ const styles = new StyleSheet.create({
     textStyle: {
         color:'#F1F8E8',
         fontSize:24
+    },
+    guessContainer: {
+        marginTop:20,
+        marginHorizontal:10,
+        alignItems:'center',
+        backgroundColor:Colors.Primary1,
+        height:100,
+        borderRadius:10,
+        justifyContent:'center',
+    },
+    guessTextStyle: {
+        color:'#F1F8E8',
+        fontSize:32,
     }
 })
